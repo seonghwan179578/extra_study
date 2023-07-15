7ee1af4ef243d710c6657745b12e46e760e4767d
Seonghwan Park <psh13179578@gmail.com>
2023-07-15 22:52:04 +0900

    filter

---
+++ js/es6/3-3. fliter array function.js
@@ -0,0 +1,12 @@
+// 배열함수 filter
+// 조건을 넣고, 조건에 대해 참인 것만 반환
+let ceoList = [
+    {name : "Larry Page", age : 23, ceo : true},
+    {name : "Tim Cook", age : 40, ceo : true},
+    {name : "Elon Musk", age : 55, ceo : false}
+]
+let data2 = ceoList.filter((item)=>{
+    // 23살인 아이템만 반환
+    return item.age == 23 // true, false로 떨어지는 조건을 넣어야함
+})
+console.log(data2)
