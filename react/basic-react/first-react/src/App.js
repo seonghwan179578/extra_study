3ae1b8737dca3c95ec56bbf6be35d9ed93a69314
Seonghwan Park <psh13179578@gmail.com>
2023-10-30 02:24:12 +0900

    Update App.js

--- react/basic-react/first-react/src/App.js
+++ react/basic-react/first-react/src/App.js
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import Box from './component/Box';
 
 function App() {
+  let counter = 0
+  const [counter2, setCounter2] = useState(0) // 여기서 0 부분은 초기값을 의미함
+  const increase = () => {
+    counter = counter + 1
+    setCounter2(counter2 + 1)
+    console.log("counter는", counter,"counter2 state는", counter2)
+  }
   return (
     <div>
-      <Box name="김" num={1}></Box>
-      <Box name="이" num={2}></Box>
-      <Box name="박" num={3}></Box>
+      <div>{counter}</div>
+      <div>state:{counter2}</div>
+      <button onClick={increase}>클릭!</button>
     </div>
     
   );
